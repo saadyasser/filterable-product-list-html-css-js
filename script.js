c4a2f0e255a3cd8bf6ad72258b4c6973e1196f26
saadyasser <saad.wadi.229@gmail.com>
2024-09-29 15:29:09 +0300

    Adding ui for search & ui with functionality for categories filter

--- script.js
+++ script.js
@@ -1,130 +1,170 @@
-const PRODUCTS = [
-    {
-      id: 1,
-      image: {
-        thumbnail: "./assets/images/image-waffle-thumbnail.jpg",
-        mobile: "./assets/images/image-waffle-mobile.jpg",
-        tablet: "./assets/images/image-waffle-tablet.jpg",
-        desktop: "./assets/images/image-waffle-desktop.jpg",
-      },
-      name: "Waffle with Berries",
-      category: "Waffle",
-      price: 6.5,
-    },
-    {
-      id: 2,
-      image: {
-        thumbnail: "./assets/images/image-creme-brulee-thumbnail.jpg",
-        mobile: "./assets/images/image-creme-brulee-mobile.jpg",
-        tablet: "./assets/images/image-creme-brulee-tablet.jpg",
-        desktop: "./assets/images/image-creme-brulee-desktop.jpg",
-      },
-      name: "Vanilla Bean Crème Brûlée",
-      category: "Crème Brûlée",
-      price: 7.0,
-    },
-    {
-      id: 3,
-      image: {
-        thumbnail: "./assets/images/image-macaron-thumbnail.jpg",
-        mobile: "./assets/images/image-macaron-mobile.jpg",
-        tablet: "./assets/images/image-macaron-tablet.jpg",
-        desktop: "./assets/images/image-macaron-desktop.jpg",
-      },
-      name: "Macaron Mix of Five",
-      category: "Macaron",
-      price: 8.0,
-    },
-    {
-      id: 4,
-      image: {
-        thumbnail: "./assets/images/image-tiramisu-thumbnail.jpg",
-        mobile: "./assets/images/image-tiramisu-mobile.jpg",
-        tablet: "./assets/images/image-tiramisu-tablet.jpg",
-        desktop: "./assets/images/image-tiramisu-desktop.jpg",
-      },
-      name: "Classic Tiramisu",
-      category: "Tiramisu",
-      price: 5.5,
-    },
-    {
-      id: 5,
-      image: {
-        thumbnail: "./assets/images/image-baklava-thumbnail.jpg",
-        mobile: "./assets/images/image-baklava-mobile.jpg",
-        tablet: "./assets/images/image-baklava-tablet.jpg",
-        desktop: "./assets/images/image-baklava-desktop.jpg",
-      },
-      name: "Pistachio Baklava",
-      category: "Baklava",
-      price: 4.0,
-    },
-    {
-      id: 6,
-      image: {
-        thumbnail: "./assets/images/image-meringue-thumbnail.jpg",
-        mobile: "./assets/images/image-meringue-mobile.jpg",
-        tablet: "./assets/images/image-meringue-tablet.jpg",
-        desktop: "./assets/images/image-meringue-desktop.jpg",
-      },
-      name: "Lemon Meringue Pie",
-      category: "Pie",
-      price: 5.0,
-    },
-    {
-      id: 7,
-      image: {
-        thumbnail: "./assets/images/image-cake-thumbnail.jpg",
-        mobile: "./assets/images/image-cake-mobile.jpg",
-        tablet: "./assets/images/image-cake-tablet.jpg",
-        desktop: "./assets/images/image-cake-desktop.jpg",
-      },
-      name: "Red Velvet Cake",
-      category: "Cake",
-      price: 4.5,
-    },
-    {
-      id: 8,
-      image: {
-        thumbnail: "./assets/images/image-brownie-thumbnail.jpg",
-        mobile: "./assets/images/image-brownie-mobile.jpg",
-        tablet: "./assets/images/image-brownie-tablet.jpg",
-        desktop: "./assets/images/image-brownie-desktop.jpg",
-      },
-      name: "Salted Caramel Brownie",
-      category: "Brownie",
-      price: 4.5,
-    },
-    {
-      id: 9,
-      image: {
-        thumbnail: "./assets/images/image-panna-cotta-thumbnail.jpg",
-        mobile: "./assets/images/image-panna-cotta-mobile.jpg",
-        tablet: "./assets/images/image-panna-cotta-tablet.jpg",
-        desktop: "./assets/images/image-panna-cotta-desktop.jpg",
-      },
-      name: "Vanilla Panna Cotta",
-      category: "Panna Cotta",
-      price: 6.5,
-    },
-  ];
+
 const productsList = document.getElementById("products-list");
+const searchInput = document.getElementById("search-input");
+const PRODUCTS = [
+  {
+    id: 1,
+    image: {
+      thumbnail: "./assets/images/image-waffle-thumbnail.jpg",
+      mobile: "./assets/images/image-waffle-mobile.jpg",
+      tablet: "./assets/images/image-waffle-tablet.jpg",
+      desktop: "./assets/images/image-waffle-desktop.jpg",
+    },
+    name: "Waffle with Berries",
+    category: "Waffle",
+    price: 6.5,
+  },
+  {
+    id: 2,
+    image: {
+      thumbnail: "./assets/images/image-creme-brulee-thumbnail.jpg",
+      mobile: "./assets/images/image-creme-brulee-mobile.jpg",
+      tablet: "./assets/images/image-creme-brulee-tablet.jpg",
+      desktop: "./assets/images/image-creme-brulee-desktop.jpg",
+    },
+    name: "Vanilla Bean Crème Brûlée",
+    category: "Crème Brûlée",
+    price: 7.0,
+  },
+  {
+    id: 3,
+    image: {
+      thumbnail: "./assets/images/image-macaron-thumbnail.jpg",
+      mobile: "./assets/images/image-macaron-mobile.jpg",
+      tablet: "./assets/images/image-macaron-tablet.jpg",
+      desktop: "./assets/images/image-macaron-desktop.jpg",
+    },
+    name: "Macaron Mix of Five",
+    category: "Macaron",
+    price: 8.0,
+  },
+  {
+    id: 4,
+    image: {
+      thumbnail: "./assets/images/image-tiramisu-thumbnail.jpg",
+      mobile: "./assets/images/image-tiramisu-mobile.jpg",
+      tablet: "./assets/images/image-tiramisu-tablet.jpg",
+      desktop: "./assets/images/image-tiramisu-desktop.jpg",
+    },
+    name: "Classic Tiramisu",
+    category: "Tiramisu",
+    price: 5.5,
+  },
+  {
+    id: 5,
+    image: {
+      thumbnail: "./assets/images/image-baklava-thumbnail.jpg",
+      mobile: "./assets/images/image-baklava-mobile.jpg",
+      tablet: "./assets/images/image-baklava-tablet.jpg",
+      desktop: "./assets/images/image-baklava-desktop.jpg",
+    },
+    name: "Pistachio Baklava",
+    category: "Baklava",
+    price: 4.0,
+  },
+  {
+    id: 6,
+    image: {
+      thumbnail: "./assets/images/image-meringue-thumbnail.jpg",
+      mobile: "./assets/images/image-meringue-mobile.jpg",
+      tablet: "./assets/images/image-meringue-tablet.jpg",
+      desktop: "./assets/images/image-meringue-desktop.jpg",
+    },
+    name: "Lemon Meringue Pie",
+    category: "Pie",
+    price: 5.0,
+  },
+  {
+    id: 7,
+    image: {
+      thumbnail: "./assets/images/image-cake-thumbnail.jpg",
+      mobile: "./assets/images/image-cake-mobile.jpg",
+      tablet: "./assets/images/image-cake-tablet.jpg",
+      desktop: "./assets/images/image-cake-desktop.jpg",
+    },
+    name: "Red Velvet Cake",
+    category: "Cake",
+    price: 4.5,
+  },
+  {
+    id: 8,
+    image: {
+      thumbnail: "./assets/images/image-brownie-thumbnail.jpg",
+      mobile: "./assets/images/image-brownie-mobile.jpg",
+      tablet: "./assets/images/image-brownie-tablet.jpg",
+      desktop: "./assets/images/image-brownie-desktop.jpg",
+    },
+    name: "Salted Caramel Brownie",
+    category: "Brownie",
+    price: 4.5,
+  },
+  {
+    id: 9,
+    image: {
+      thumbnail: "./assets/images/image-panna-cotta-thumbnail.jpg",
+      mobile: "./assets/images/image-panna-cotta-mobile.jpg",
+      tablet: "./assets/images/image-panna-cotta-tablet.jpg",
+      desktop: "./assets/images/image-panna-cotta-desktop.jpg",
+    },
+    name: "Vanilla Panna Cotta",
+    category: "Panna Cotta",
+    price: 6.5,
+  },
+];
 
-for(let i = 0; i < PRODUCTS.length; i++) {
 
-    const product = document.createElement("article");
 
-    const productImage = document.createElement("img");
-    productImage.src = PRODUCTS[i].image.desktop
-    
-    const productTitle = document.createElement("h2");
-    productTitle.textContent = PRODUCTS[i].name;
-    
-    product.appendChild(productImage);
-    product.appendChild(productTitle);
-    
-    productsList.append(product)
+const showProducts =  async (products, searchKeyword = "", category = 'All') => {
+  productsList.style.display = 'none';
+
+    let filteredProduct = JSON.parse(JSON.stringify(products));
+    filteredProduct = getFilteredProducts(PRODUCTS, searchKeyword, category);
+
+  for(let i = 0; i < filteredProduct.length; i++) {
+    productsList.style.display = 'grid';
+
+      const product = document.createElement("article");
+
+      const productImage = document.createElement("img");
+      productImage.src = filteredProduct[i].image.mobile
+      
+      const productTitle = document.createElement("h2");
+      productTitle.textContent = filteredProduct[i].name;
+      
+      const productCategory = document.createElement("span");
+      productCategory.textContent = filteredProduct[i].category;
+
+      product.appendChild(productImage);
+      product.appendChild(productTitle);
+      product.appendChild(productCategory);
+      
+      productsList.append(product)
+  }
+}
+
+window.onload = () => {
+  showProducts(PRODUCTS);
+}
+// showProducts(PRODUCTS);
+
+const categoryInput = document.getElementById('category');
+categoryInput.onchange = (e) => {
+  const articles = Array.from(document.querySelectorAll('article')) 
+  for(let i = 0; i < articles.length; i++) {
+    productsList.removeChild(document.querySelector('article'))
+  }
+  showProducts(PRODUCTS, '', e.target.value);
+
+}
+
+const getFilteredProducts = (products, searchKeyword, category) => {
+  if(searchKeyword || category !== 'All') {
+    return PRODUCTS.filter(product => product.name.includes(searchKeyword) && product.category === category)
+  }
+  return products
 }
 
 
 
+
+  
